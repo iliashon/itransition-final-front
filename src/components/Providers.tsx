5d9bf70c31ff2e1ab9a00aa7c71f718fa5bb4e3d
Tupalski Ilya <itupalski@gmail.com>
2024-03-06 15:23:41 +0100

    fix: type children in providers component (Wed Mar  6 15:23:41 CET 2024)

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -3,14 +3,11 @@
 import { ReactNode } from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProvider as MaterialTailwindProvider } from "@material-tailwind/react";
-import { ReactNodeLike } from "prop-types";
 
-export default function Providers({ children }: { children: ReactNode }) {
+export default function Providers({ children }: { children: any }) {
     return (
-        <MaterialTailwindProvider>
-            <NextThemesProvider attribute="class" defaultTheme="dark">
-                {children}
-            </NextThemesProvider>
-        </MaterialTailwindProvider>
+        <NextThemesProvider attribute="class" defaultTheme="dark">
+            <MaterialTailwindProvider>{children}</MaterialTailwindProvider>
+        </NextThemesProvider>
     );
 }
