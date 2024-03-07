eba27bcce74d970501964dce51a7c14b76177e13
Tupalski Ilya <itupalski@gmail.com>
2024-03-07 11:10:30 +0100

    feat: add validation on form sign up (Thu Mar  7 11:10:30 CET 2024)

--- src/components/auth/SignUpForm.tsx
+++ src/components/auth/SignUpForm.tsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
 import { IoEyeOffOutline, IoEyeOutline } from "react-icons/io5";
-import { useForm } from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
 import TSignInForm from "@/types/auth/TSignInForm";
+import TLogInForm from "@/types/auth/TLogInForm";
+import { Alert } from "@material-tailwind/react";
+import { PiWarningCircle } from "react-icons/pi";
+
+const REGEX_EMAIL = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
 
 const style = {
     input: "h-12 px-4 rounded-lg text-lg bg-transparent border border-black focus:outline-none w-full",
@@ -16,36 +21,95 @@ export default function SignUpForm() {
         formState: { errors },
     } = useForm<TSignInForm>();
 
+    const onSubmitForm: SubmitHandler<TLogInForm> = (data) => {
+        console.log(data);
+    };
+
     const handleShowPass = () => {
         setIsHiddenPass(!isHiddenPass);
     };
 
     return (
-        <form className="flex flex-col gap-5">
+        <form
+            className="flex flex-col gap-5"
+            onSubmit={handleSubmit(onSubmitForm)}
+        >
             <input
                 type="text"
                 placeholder="First name"
-                {...register("first_name")}
-                className={`${style.input}`}
+                {...register("first_name", {
+                    required: "This field is required",
+                })}
+                className={`${style.input} ${errors.first_name ? "border-red-500" : ""}`}
             />
+            {errors.first_name ? (
+                <Alert
+                    variant="ghost"
+                    color="red"
+                    icon={<PiWarningCircle className="h-6 w-6" />}
+                    className="-mt-3 py-2"
+                >
+                    {errors.first_name.message}
+                </Alert>
+            ) : (
+                ""
+            )}
             <input
                 type="text"
                 placeholder="Last name"
-                {...register("last_name")}
-                className={`${style.input}`}
+                {...register("last_name", {
+                    required: "This field is required",
+                })}
+                className={`${style.input} ${errors.last_name ? "border-red-500" : ""}`}
             />
+            {errors.last_name ? (
+                <Alert
+                    variant="ghost"
+                    color="red"
+                    icon={<PiWarningCircle className="h-6 w-6" />}
+                    className="-mt-3 py-2"
+                >
+                    {errors.last_name.message}
+                </Alert>
+            ) : (
+                ""
+            )}
             <input
                 type="text"
                 placeholder="Email"
-                {...register("email")}
-                className={`${style.input}`}
+                {...register("email", {
+                    required: "This field is required",
+                    pattern: {
+                        value: REGEX_EMAIL,
+                        message: "Check mail format example@gmail.com",
+                    },
+                })}
+                className={`${style.input} ${errors.email ? "border-red-500" : ""}`}
             />
+            {errors.email ? (
+                <Alert
+                    variant="ghost"
+                    color="red"
+                    icon={<PiWarningCircle className="h-6 w-6" />}
+                    className="-mt-3 py-2"
+                >
+                    {errors.email.message}
+                </Alert>
+            ) : (
+                ""
+            )}
             <div className="relative">
                 <input
                     type={isHiddenPass ? "password" : "text"}
                     placeholder="Password"
-                    {...register("password")}
-                    className={`${style.input} pr-10`}
+                    {...register("password", {
+                        required: "This field is required",
+                        minLength: {
+                            value: 8,
+                            message: "Minimum 8 characters",
+                        },
+                    })}
+                    className={`${style.input} pr-10 ${errors.password ? "border-red-500" : ""}`}
                 />
                 {isHiddenPass ? (
                     <IoEyeOffOutline
@@ -57,6 +121,18 @@ export default function SignUpForm() {
                         className="h-6 w-6 absolute right-3 top-3 cursor-pointer"
                         onClick={handleShowPass}
                     />
+                )}
+                {errors.password ? (
+                    <Alert
+                        variant="ghost"
+                        color="red"
+                        icon={<PiWarningCircle className="h-6 w-6" />}
+                        className="mt-2 py-2"
+                    >
+                        {errors.password.message}
+                    </Alert>
+                ) : (
+                    ""
                 )}
             </div>
             <button className="bg-black text-white h-12 rounded-lg font-medium text-lg hover:opacity-70 duration-300">
