9d89366aaa50970e8900643534c0b7179ac7d12f
Tupalski Ilya <itupalski@gmail.com>
2024-03-21 10:56:43 +0100

    fix: delete redirectRefresh func (Thu Mar 21 10:56:42 CET 2024)

--- src/utils/redirectRefresh.ts
+++
@@ -1,7 +0,0 @@
-import { useRouter } from "next/navigation";
-
-export default function redirectRefresh(path: string) {
-    const router = useRouter();
-    router.push(path);
-    router.refresh();
-}
