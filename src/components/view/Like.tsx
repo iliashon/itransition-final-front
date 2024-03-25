2b833683f220f577712fbb47822d2248ef42bd1f
Tupalski Ilya <itupalski@gmail.com>
2024-03-25 11:54:02 +0100

    fix: add notification on like if not auth (Mon Mar 25 11:54:02 CET 2024)

--- src/components/view/Like.tsx
+++ src/components/view/Like.tsx
@@ -4,6 +4,7 @@ import { FaHeart } from "react-icons/fa";
 import { useEffect, useState } from "react";
 import LikeService from "@/services/like.service";
 import TLikeData from "@/types/like/TLikeData";
+import { Alert } from "@material-tailwind/react";
 
 export default function Like({
     item_id,
@@ -14,12 +15,20 @@ export default function Like({
 }) {
     const [likes, setLikes] = useState<TLikeData[]>();
     const [activeLikeId, setActiveLikeId] = useState<number | undefined>();
+    const [notAuthError, setNotAuthError] = useState(false);
 
     const searchLike = () => {
         const searchLike = likes?.find((like) => like.user_id === user_id);
         !!searchLike
             ? setActiveLikeId(searchLike.id)
             : setActiveLikeId(undefined);
+    };
+
+    const handleNotAuthError = () => {
+        setNotAuthError(true);
+        setTimeout(() => {
+            setNotAuthError(false);
+        }, 3000);
     };
 
     const handleDeleteLike = async () => {
@@ -43,18 +52,25 @@ export default function Like({
     }, [likes]);
 
     return (
-        <div className="flex gap-2">
-            <FaHeart
-                onClick={
-                    user_id
-                        ? activeLikeId
-                            ? handleDeleteLike
-                            : handleCreateLike
-                        : () => null
-                }
-                className={`${activeLikeId ? "opacity-100" : "opacity-50"} h-6 w-6 text-red-500 hover:opacity-100 cursor-pointer duration-300 active:scale-75`}
-            />
-            <span className="font-semibold">{likes?.length}</span>
-        </div>
+        <>
+            <div className="flex gap-2">
+                <FaHeart
+                    onClick={
+                        user_id
+                            ? activeLikeId
+                                ? handleDeleteLike
+                                : handleCreateLike
+                            : handleNotAuthError
+                    }
+                    className={`${activeLikeId ? "opacity-100" : "opacity-50"} h-6 w-6 text-red-500 hover:opacity-100 cursor-pointer duration-300 active:scale-75`}
+                />
+                <span className="font-semibold">{likes?.length}</span>
+            </div>
+            {notAuthError && (
+                <Alert className="absolute bottom-2 left-2 w-72" color="red">
+                    You are not authorized
+                </Alert>
+            )}
+        </>
     );
 }
