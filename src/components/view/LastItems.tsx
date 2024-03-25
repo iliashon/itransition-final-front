98f967e69a8e7a46f75a224caac8186f8d2a8189
Tupalski Ilya <itupalski@gmail.com>
2024-03-25 15:01:44 +0100

    fix: style home page (Mon Mar 25 15:01:44 CET 2024)

--- src/components/view/LastItems.tsx
+++ src/components/view/LastItems.tsx
@@ -3,26 +3,56 @@ import Link from "next/link";
 
 export default function LastItems({ data }: { data: TItemData[] }) {
     return (
-        <section className="px-10 pt-10 grid grid-cols-5 gap-5">
-            {data.map((item) => {
-                return (
-                    <article key={item.id}>
-                        <div className="h-48 w-full overflow-hidden rounded-lg bg-white">
+        <section className="px-0 lg:px-10 pt-10 grid lg:grid-cols-3 lg:grid-rows-2 grid-cols-1 gap-5">
+            {data.map((item, index) => {
+                if (index === 2) {
+                    return (
+                        <article
+                            key={item.id}
+                            className="row-span-2 h-60 lg:h-full w-full overflow-hidden rounded-lg relative"
+                        >
                             <img
                                 src={item.image_url || "/imageNotFound.jpeg"}
                                 alt={item.name}
                                 className="h-full w-full object-cover object-center"
                             />
-                        </div>
-                        <div className="flex flex-col items-start gap-5 mt-5">
-                            <h2 className="font-bold text-2xl">{item.name}</h2>
+                            <div className="absolute z-10 bottom-10 left-5 flex flex-col items-start gap-5">
+                                <h2 className="font-bold text-2xl text-white">
+                                    {item.name}
+                                </h2>
+                                <Link
+                                    href={`/collection/${item.id}`}
+                                    className="uppercase font-semibold text-[12px] text-white text-center bg-black dark:bg-white dark:text-black duration-300 rounded-lg py-2 px-3"
+                                >
+                                    Read more
+                                </Link>
+                            </div>
+                            <div className="absolute bg-black/50 hover:bg-black/30 duration-200 top-0 bottom-0 left-0 right-0"></div>
+                        </article>
+                    );
+                }
+                return (
+                    <article
+                        key={item.id}
+                        className="h-60 w-full overflow-hidden rounded-lg bg-white relative"
+                    >
+                        <img
+                            src={item.image_url || "/imageNotFound.jpeg"}
+                            alt={item.name}
+                            className="h-full w-full object-cover object-center"
+                        />
+                        <div className="absolute z-10 bottom-10 left-5 flex flex-col items-start gap-5">
+                            <h2 className="font-bold text-2xl text-white">
+                                {item.name}
+                            </h2>
                             <Link
-                                href={`/item/${item.id}`}
+                                href={`/collection/${item.id}`}
                                 className="uppercase font-semibold text-[12px] text-white text-center bg-black dark:bg-white dark:text-black duration-300 rounded-lg py-2 px-3"
                             >
                                 Read more
                             </Link>
                         </div>
+                        <div className="absolute bg-black/50 hover:bg-black/30 duration-200 top-0 bottom-0 left-0 right-0"></div>
                     </article>
                 );
             })}
--- src/components/view/TopFiveCollection.tsx
+++ src/components/view/TopFiveCollection.tsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import TCollectionData from "@/types/collection/TCollectionData";
+import Image from "next/image";
 
 export default function TopFiveCollection({
     data,
@@ -9,11 +10,14 @@ export default function TopFiveCollection({
     data: TCollectionData[];
 }) {
     return (
-        <section className="px-10 pt-10 grid grid-cols-5 gap-5">
-            {data.map((collection) => {
-                return (
-                    <article key={collection.id}>
-                        <div className="h-48 w-full overflow-hidden rounded-lg bg-white">
+        <section className="px-0 lg:px-10 pt-10 grid lg:grid-cols-3 lg:grid-rows-2 grid-cols-1 gap-5">
+            {data.map((collection, index) => {
+                if (index === 0) {
+                    return (
+                        <article
+                            key={collection.id}
+                            className="row-span-2 h-60 lg:h-full w-full overflow-hidden rounded-lg relative"
+                        >
                             <img
                                 src={
                                     collection.image_url ||
@@ -22,9 +26,33 @@ export default function TopFiveCollection({
                                 alt={collection.name}
                                 className="h-full w-full object-cover object-center"
                             />
-                        </div>
-                        <div className="flex flex-col items-start gap-5 mt-5">
-                            <h2 className="font-bold text-2xl">
+                            <div className="absolute z-10 bottom-10 left-5 flex flex-col items-start gap-5">
+                                <h2 className="font-bold text-2xl text-white">
+                                    {collection.name}
+                                </h2>
+                                <Link
+                                    href={`/collection/${collection.id}`}
+                                    className="uppercase font-semibold text-[12px] text-white text-center bg-black dark:bg-white dark:text-black duration-300 rounded-lg py-2 px-3"
+                                >
+                                    Read more
+                                </Link>
+                            </div>
+                            <div className="absolute bg-black/50 hover:bg-black/30 duration-200 top-0 bottom-0 left-0 right-0"></div>
+                        </article>
+                    );
+                }
+                return (
+                    <article
+                        key={collection.id}
+                        className="h-60 w-full overflow-hidden rounded-lg bg-white relative"
+                    >
+                        <img
+                            src={collection.image_url || "/imageNotFound.jpeg"}
+                            alt={collection.name}
+                            className="h-full w-full object-cover object-center"
+                        />
+                        <div className="absolute z-10 bottom-10 left-5 flex flex-col items-start gap-5">
+                            <h2 className="font-bold text-2xl text-white">
                                 {collection.name}
                             </h2>
                             <Link
@@ -34,6 +62,7 @@ export default function TopFiveCollection({
                                 Read more
                             </Link>
                         </div>
+                        <div className="absolute bg-black/50 hover:bg-black/30 duration-200 top-0 bottom-0 left-0 right-0"></div>
                     </article>
                 );
             })}
